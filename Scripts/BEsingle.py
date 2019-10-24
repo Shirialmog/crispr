b863a5081868d60b2d2b2f69f7663ff1cc948554
Shirialmog <shirlool@gmail.com>
2019-10-24 22:35:38 +0300

    oct24

--- Scripts/BEsingle.py
+++ Scripts/BEsingle.py
@@ -25,8 +25,8 @@ def matchBE(snp, BElist):
                 j=0
                 while (j<=lenPAM):
                     if sequence[temp] in BEletter[PAM[j]]:
-                        j=j+1
-                        temp=temp+1
+                        j+=1
+                        temp+=1
                     else:
                         break
                     if j == len(PAM):
@@ -36,7 +36,7 @@ def matchBE(snp, BElist):
                 matches_list.append(BE)
     return matches_list
 
-def cleanMatch(snp, Matches, BElist,rev):
+def cleanMatch(snp,Matches, BElist,rev):
     printRevSeq=None
     printRevCorSeq=None
     clean_dic={}
@@ -49,10 +49,8 @@ def cleanMatch(snp, Matches, BElist,rev):
             seq3 = snp.seq3
             seq5=snp.seq5
         else:
-            seq3 = snp.seq5
-            seq5 = snp.seq3
-            seq3 = seq3[::-1]  # get reverse
-            seq5 = seq5[::-1]  # get reverse
+            seq3 = snp.seq5[::-1] # get reverse
+            seq5 = snp.seq3[::-1]  # get reverse
 
         PAM=BElist[BE][0]
         start=BElist[BE][1]-1
@@ -67,8 +65,8 @@ def cleanMatch(snp, Matches, BElist,rev):
             j=0
             while (j<=lenPAM):
                 if seq3[temp] in BEletter[PAM[j]]:
-                    j=j+1
-                    temp=temp+1
+                    j+=1
+                    temp+=1
                 else:
                     break
                 if j == len(PAM):
@@ -76,7 +74,7 @@ def cleanMatch(snp, Matches, BElist,rev):
                     break
         locations_dic[BE]=locations_list
 
-        totalSeq1=seq5+snp.mutation+seq3
+        totalSeq1 = seq5 + snp.mutation + seq3
         totalSeq2 = seq5 + snp.wt + seq3
         printSeq5=seq5 #for results
         printSeq3=seq3  # for results
@@ -123,11 +121,11 @@ def cleanMatch(snp, Matches, BElist,rev):
             totalSeq2 = totalSeq2[:-2]
             printSeq3 = printSeq3[:-2]
         totalSeq2=Seq(totalSeq2)
-        print ("total: ", totalSeq2)
-        protein_seq=totalSeq2.translate()
+        if rev==False:
+            protein_seq=totalSeq2.translate()
+        else:
+            protein_seq=totalSeq2.reverse_complement().translate()
 
-        max_num = 0
-        protein_match=False
         clean_list = []
         quiet_list=[]
         origMutSeq = {}
@@ -135,9 +133,9 @@ def cleanMatch(snp, Matches, BElist,rev):
         printRevCorSeq = {}
         finalShowSeq = {}
         for loc in locations_dic[BE]:
+            protein_match = False
             locFromEnd=len(printSeq3)-loc
             loc=loc+len(printSeq5)
-            #activation_window = totalSeq1[loc-end-diff:loc-start]
             activation_window=totalSeq1[len(printSeq3)-locFromEnd+len(printSeq5)-end:len(printSeq3)-locFromEnd+len(printSeq5)-start+1]
             num = 0  # number of times the variant appears within the activation window
             max_num=0
@@ -155,8 +153,7 @@ def cleanMatch(snp, Matches, BElist,rev):
 
             if rev==False:
                 new = new_AW
-                finalSeq = totalSeq1[0:loc - end - diff] + str(new) + totalSeq1[loc - start+1:]
-                print ("Final:", finalSeq)
+                finalSeq = Seq(totalSeq1[0:loc - end] + str(new) + totalSeq1[loc - start+1:])
                 beginningP = Seq(
                     totalSeq1[0:len(printSeq3) - locFromEnd + len(printSeq5) - end])
                 endP = Seq(totalSeq1[len(printSeq3) - locFromEnd + len(printSeq5) - start + 1:])
@@ -167,21 +164,24 @@ def cleanMatch(snp, Matches, BElist,rev):
                 #printPam[loc] = printPamSeq(printSeq5, new_AW, printSeq3, locFromEnd, PAM)
                 printPam[loc] = printPamSeq(beginningP, new_AW, endP, locFromEnd, PAM)
             else:
-                finalSeq = totalSeq1[0:loc - end - diff] +new_AW + totalSeq1[loc - start+1:]
+                finalSeq2 = totalSeq1[0:loc - end] +new_AW + totalSeq1[loc - start+1:]
+                print (finalSeq2)
                 #beginningP=Seq(totalSeq1[0:loc - end - diff]).reverse_complement()
                 beginningP=Seq(totalSeq1[0:len(printSeq3)-locFromEnd+len(printSeq5)-end]).reverse_complement()
                 old_AW=Seq(activation_window).reverse_complement()
                 new_AW=Seq(new_AW).reverse_complement()
                 #endP=Seq(totalSeq1[loc - start:]).reverse_complement()
                 endP=Seq(totalSeq1[len(printSeq3)-locFromEnd+len(printSeq5)-start+1:]).reverse_complement()
+                finalSeq = endP + new_AW + beginningP
+                print (finalSeq)
                 oldShowSeq = endP + "<b>" + old_AW + "</b>" + beginningP
                 finalShowSeq[loc] = endP + "<class style='color:blue'><b>" + new_AW + "</b></class>" + beginningP
                 origMutSeq[loc] = Seq(printSeq3).reverse_complement() + "<b>" + Seq(snp.mutation).reverse_complement() + "</b>" + Seq(printSeq5).reverse_complement()
                 origRevSeq=printSeq5+"<b>"+snp.mutation+"</b>"+printSeq3
                 printRevCorSeq[loc]= printPamSeq(beginningP.reverse_complement(), new_AW.reverse_complement(), endP.reverse_complement(), locFromEnd, PAM)
                 printPam[loc]= printPamSeq(beginningP.reverse_complement(), old_AW.reverse_complement(), endP.reverse_complement(), locFromEnd, PAM)
-
-            protein_seq_new=Seq(finalSeq).translate()
+            print ("f:",finalSeq)
+            protein_seq_new=finalSeq.translate()
             if protein_seq==protein_seq_new:
                 protein_match=True
 
@@ -189,13 +189,101 @@ def cleanMatch(snp, Matches, BElist,rev):
                 clean_list.append(loc)
             if protein_match == True:
                 quiet_list.append(loc)
-        #print (BE, quiet_list)
         clean_dic[BE]=[origMutSeq,printPam,printRevCorSeq,finalShowSeq,PAM,clean_list,rev]
 
-
+        quiet_list=quietCheck(snp,seq5,seq3,locations_dic,rev,BE,start,end)
         quiet_dic[BE]=[origMutSeq,printPam,printRevCorSeq,finalShowSeq,PAM, quiet_list,rev]
 
     return clean_dic,quiet_dic,origMutSeq,origRevSeq,locations_dic
+
+def quietCheck(snp,seq5,seq3,locations_dic,rev,BE,start,end):
+    quiet_list = []
+    totalSeq1 = seq5 + snp.mutation + seq3
+    totalSeq2 = seq5 + snp.wt + seq3
+    printSeq5 = seq5  # for results
+    printSeq3 = seq3  # for results
+    diff = 0
+    if snp.readingFrame == "1":
+        if len(snp.seq5) % 3 == 1:
+            totalSeq1 = totalSeq1[1:]
+            totalSeq2 = totalSeq2[1:]
+            printSeq5 = printSeq5[1:]
+            diff = 1
+        elif len(snp.seq5) % 3 == 2:
+            totalSeq1 = totalSeq1[2:]
+            totalSeq2 = totalSeq2[2:]
+            printSeq5 = printSeq5[2:]
+            diff = 2
+    elif snp.readingFrame == "2":
+        if len(snp.seq5) % 3 == 0:
+            totalSeq1 = totalSeq1[2:]
+            totalSeq2 = totalSeq2[2:]
+            printSeq5 = printSeq5[2:]
+            diff = 2
+        elif len(snp.seq5) % 3 == 2:
+            totalSeq1 = totalSeq1[1:]
+            totalSeq2 = totalSeq1[2:]
+            printSeq5 = printSeq5[1:]
+            diff = 1
+    elif snp.readingFrame == "3":
+        if len(snp.seq5) % 3 == 0:
+            totalSeq1 = totalSeq1[1:]
+            totalSeq2 = totalSeq2[1:]
+            printSeq5 = printSeq5[1:]
+            diff = 1
+        elif len(snp.seq5) % 3 == 1:
+            totalSeq1 = totalSeq1[2:]
+            totalSeq2 = totalSeq2[2:]
+            printSeq5 = printSeq5[2:]
+            diff = 2
+    if len(totalSeq1) % 3 == 1:
+        totalSeq1 = totalSeq1[:-1]
+        totalSeq2 = totalSeq2[:-1]
+        printSeq3 = printSeq3[:-1]
+    if len(totalSeq1) % 3 == 2:
+        totalSeq1 = totalSeq1[:-2]
+        totalSeq2 = totalSeq2[:-2]
+        printSeq3 = printSeq3[:-2]
+    totalSeq2 = Seq(totalSeq2)
+
+
+    for loc in locations_dic[BE]:
+        protein_match = False
+        locFromEnd = len(printSeq3) - loc
+        loc = loc + len(printSeq5)
+        activation_window = totalSeq1[
+                            len(printSeq3) - locFromEnd + len(printSeq5) - end:len(printSeq3) - locFromEnd + len(
+                                printSeq5) - start + 1]
+
+        new_AW = []
+        for i in range(len(activation_window)):
+            if activation_window[i] == snp.mutation:
+                new_AW.append(snp.wt)
+            else:
+                new_AW.append(activation_window[i])
+        new_AW = ''.join(new_AW)
+
+
+        if rev == False:
+            new = new_AW
+            finalSeq = Seq(totalSeq1[0:loc - end] + str(new) + totalSeq1[loc - start + 1:])
+            protein_seq = totalSeq2.translate()
+        else:
+            beginningP = Seq(totalSeq1[0:len(printSeq3) - locFromEnd + len(printSeq5) - end]).reverse_complement()
+            old_AW = Seq(activation_window).reverse_complement()
+            new_AW = Seq(new_AW).reverse_complement()
+            # endP=Seq(totalSeq1[loc - start:]).reverse_complement()
+            endP = Seq(totalSeq1[len(printSeq3) - locFromEnd + len(printSeq5) - start + 1:]).reverse_complement()
+            finalSeq = endP + new_AW + beginningP
+            protein_seq = totalSeq2.reverse_complement().translate()
+
+        protein_seq_new = finalSeq.translate()
+        if protein_seq == protein_seq_new:
+            protein_match = True
+        if protein_match == True:
+            quiet_list.append(loc)
+
+    return quiet_list
 
 def printPamSeq(seq5,activationWindow,seq3, locfromEnd,PAM):
     len3=len(seq3)
@@ -211,6 +299,50 @@ def getRevComp(snp):
     seq3=str(rc_seq[len5+1:])
     new_snp=BEseqType(snp.snpID,seq5 ,seq3,snp.mutation,snp.wt, snp.readingFrame, snp.aaPosition,snp.geneName,snp.geneID)
     return new_snp
+
+
+def checkRF(snp):
+    #this function will check the other 2 bases in the reading frame to see whether fixing them may result in a quiet result
+    if snp.readingFrame==1:
+        first_seq5=snp.seq5+snp.mutation
+        first_mutation=snp.seq3[0]
+        first_wt=str(Seq(snp.mutation).reverse_complement())
+        first_seq3=snp.seq3[1:]
+        snp1=BEseqType(12,first_seq5,first_seq3,first_wt,first_mutation,2,1,1,1)
+        second_seq5 = snp.seq5 + snp.mutation+snp.seq3[0]
+        second_mutation = snp.seq3[1]
+        second_wt = str(Seq(snp.mutation).reverse_complement())
+        second_seq3 = snp.seq3[2:]
+        snp2 = BEseqType(12, second_seq5, second_seq3, second_wt,second_mutation, 3,1,1,1)
+        return snp1,snp2
+
+    elif snp.readingFrame==2:
+        first_seq5=snp.seq5[:-1]
+        first_mutation=snp.seq5[-1]
+        first_wt=str(Seq(snp.mutation).reverse_complement())
+        first_seq3=snp.mutation+snp.seq3
+        snp1=BEseqType(12,first_seq5,first_seq3,first_wt,first_mutation,1,1,1,1)
+        second_seq5 = snp.seq5 + snp.mutation
+        second_mutation = snp.seq3[0]
+        second_wt = str(Seq(snp.mutation).reverse_complement())
+        second_seq3 = snp.seq3[1:]
+        snp2 = BEseqType(12, second_seq5, second_seq3, second_wt,second_mutation, 3,1,1,1)
+        return snp1,snp2
+
+    # elif snp.readingFrame==3:
+    else:
+        first_seq5=snp.seq5[:-2]
+        first_mutation=snp.seq5[-2]
+        first_wt=str(Seq(snp.mutation).reverse_complement())
+        first_seq3=snp.seq5[-1]+snp.mutation+snp.seq3
+        snp1=BEseqType(12,first_seq5,first_seq3,first_wt,first_mutation,1,1,1,1)
+        second_seq5 = snp.seq5[:-1]
+        second_mutation = snp.seq3[-1]
+        second_wt = str(Seq(snp.mutation).reverse_complement())
+        second_seq3 = snp.mutation+snp.seq3
+        snp2 = BEseqType(12, second_seq5, second_seq3,second_wt, second_mutation,2,1,1,1)
+        return snp1,snp2
+
 
 def MainBE(upSeq, downSeq, mutation, wt, readingFrame=2):
     upSeq=upSeq.upper()
@@ -241,16 +373,59 @@ def MainBE(upSeq, downSeq, mutation, wt, readingFrame=2):
         snp.wt="T"
         BElist = CBElist
         MinorBElist = CBElistMinor
-    else:
-        return ("Cannot be fixed with this tool")
 
+    snp2,snp3=checkRF(snp)
+    print (2,3)
+    if snp2.mutation == "C":
+        BElist2 = CBElist
+    elif snp2.mutation == "A":
+        BElist2 = ABElist
+    elif snp2.mutation == "T":
+        snp2 = getRevComp(snp)
+        rev = True
+        snp2.mutation = "A"
+        snp2.wt = "G"
+        BElist2 = ABElist
+    elif snp2.mutation == "G":
+        snp2 = getRevComp(snp)
+        rev = True
+        snp2.mutation = "C"
+        snp2.wt = "T"
+        BElist2 = CBElist
 
-    # check for matches in major window
-    check_match = matchBE(snp, BElist)
-
-    # check for clean match
-    clean_dic,quiet_dic,origMutSeq, origRevSeq,locations_dic= cleanMatch(snp, check_match, BElist,rev)
-    #print (clean_dic)
+    if snp3.mutation == "C":
+        BElist3 = CBElist
+    elif snp3.mutation == "A":
+        BElist3 = ABElist
+    elif snp3.mutation == "T":
+        snp3 = getRevComp(snp)
+        rev = True
+        snp3.mutation = "A"
+        snp3.wt = "G"
+        BElist3 = ABElist
+    elif snp3.mutation == "G":
+        snp3 = getRevComp(snp)
+        rev = True
+        snp3.mutation = "C"
+        snp3.wt = "T"
+        BElist3 = CBElist
+    try:
+        #check for matches in major window
+        check_match = matchBE(snp, BElist)
+        check_match2=matchBE(snp2,BElist2)
+        check_match3=matchBE(snp3,BElist3)
+        #check for clean match
+        clean_dic,quiet_dic,origMutSeq, origRevSeq,locations_dic= cleanMatch(snp, check_match, BElist,rev)
+        clean_dic2, quiet_dic2, origMutSeq2, origRevSeq2, locations_dic2 = cleanMatch(snp2, check_match2, BElist, rev)
+        clean_dic3, quiet_dic3, origMutSeq3, origRevSeq3, locations_dic3 = cleanMatch(snp3, check_match3, BElist, rev)
+        for key in quiet_dic2:
+            if key not in quiet_dic:
+                quiet_dic[key]=quiet_dic2[key]
+        for key in quiet_dic3:
+            if key not in quiet_dic:
+                quiet_dic[key]=quiet_dic3[key]
+    except:
+        return [],[],[],[],[],[],[]
     return clean_dic, quiet_dic,refSeq,mutSeq, origMutSeq, origRevSeq,locations_dic
 
 
--- Scripts/baseEditors.py
+++ Scripts/baseEditors.py
@@ -1,18 +1,20 @@
 CBElist={
     #name: [ PAM, major activity window, start ,end]
-    "BE1": ["NGG",13,17,"C","T", "U"],
-    "BE2": ["NGG",13,17,"C","T","U"],
-    "BE3": ["NGG",13,17,"C","T","U"],
-    "HF-BE3": ["NGG",13,17,"C","T","U"],
-    "BE4(max)": ["NGG",13,17,"C","T","U"],
-    "BE4-Gam": ["NGG",13,17,"C","T","U"],
+    # "BE1": ["NGG",13,17,"C","T", "U"],
+    # "BE2": ["NGG",13,17,"C","T","U"],
+    # "BE3": ["NGG",13,17,"C","T","U"],
+    # "HF-BE3": ["NGG",13,17,"C","T","U"],
+    # "BE4(max)": ["NGG",13,17,"C","T","U"],
+    # "BE4-Gam": ["NGG",13,17,"C","T","U"],
+    "BE1, BE2, BE3, HF-BE3, BE4(max), BE4-Gam": ["NGG",13,17,"C","T","U"],
     "YE1-BE3": ["NGG",14,16,"C","T","U"],
     "YEE-BE3": ["NGG",15,15,"C","T","U"],
     "VQR-BE3": ["NGAN",10,17,"C","T","U"],
     "VRER-BE3": ["NGCG",11,18,"C","T","U"],
-    "SaBE3": ["NNGRRT",10,19,"C","T","U"],
-    "SaBE4": ["NNGRRT",10,19,"C","T","U"],
-    "SaBE4-Gam": ["NNGRRT",10,19,"C","T","U"],
+    # "SaBE3": ["NNGRRT",10,19,"C","T","U"],
+    # "SaBE4": ["NNGRRT",10,19,"C","T","U"],
+    # "SaBE4-Gam": ["NNGRRT",10,19,"C","T","U"],
+    "SaBE3, SaBE4, SaBE-Gam": ["NNGRRT",10,19,"C","T","U"],
     "Sa(KKH)-BE3": ["NNNRRT",10,19,"C","T","U"],
     "Cas12a-BE": ["TTTV",10,12,"C","T","D"],
     "Target-AID": ["NGG",17,19,"C","T","U"],
@@ -20,6 +22,9 @@ CBElist={
     "xBE3": ["NG",13,17,"C","T","U"],
     "eA3A-BE3": ["NGG",13,17,"C","T","U"], #special case, only when C comes after T
     "BE-PLUS": ["NGG",7,17,"C","T","U"],
+    "CP-CBEmax variants": ["NGG",12,17,"C","T","U"],
+    "evoAPOBEC1-BE4max, evoFERNY-BE4max": ["NGG",13,18,"C","T","U"],
+    "evoCDA1-BE4max": ["NGG",9,20,"C","T","U"]
 }
 CBElistMinor={
     #name: [ PAM, major activity window, start ,end]
@@ -49,11 +54,12 @@ ABElist={
     "ABE 7.9": ["NGG",13,16, "A","G","U"],
     "ABE 7.10": ["NGG",14,17, "A","G","U"],
     "ABE 7.10*": ["NGG",13,17, "A","G","U"],
-    "xABE": ["NG",14,17, "A","G","U"],
+    "xABE, NG-ABEmax": ["NG",14,17, "A","G","U"],
     "ABESa": ["NNGRRT",10,16, "A","G","U"],
     "VQR-ABE": ["NGA",15,17, "A","G","U"],
     "VRER-ABE": ["NGCG",15,17, "A","G","U"],
-    "Sa(KKH)-ABE": ["NNNRRT",10,16, "A","G","U"]
+    "Sa(KKH)-ABE": ["NNNRRT",10,16, "A","G","U"],
+    "CP-ABEmax variants":["NGG",12,17, "A","G","U"]
 }
 ABElistMinor={
     #name: [ PAM, major activity window, start ,end]
